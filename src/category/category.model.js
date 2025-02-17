a5cbe5568f3bf52c6b9af737c06fce852c1ab594
Jonathan Gutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-17 08:02:19 -0600

    Category model added

---
+++ src/category/category.model.js
@@ -0,0 +1,21 @@
+const categorySchema = Schema (
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+            unique: true,
+            maxLength: [25, `Can't be oversize 25 characters`]
+        },
+        description: {
+            type: String,
+            required: [true, 'Category description is required'],
+            maxLength: [50, `Can't be oversize 50 characters`]
+        }
+    },
+    {
+        versionKey: false, 
+        timeStamps: true
+    }
+)
+
+export default model('Category', categorySchema)
