14634dad61f25b93fb75973c800c95d1ac2538ff
Jonathan Gutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-17 09:03:17 -0600

    Function to create a default category added

---
+++ src/category/category.controller.js
@@ -0,0 +1,41 @@
+import Category from "../category/category.model" 
+
+export const defaultCategory = async () => {
+    try {
+        const defaultCategory = await Category.findOne({ name: 'default' })
+
+        if (!defaultCategory) {
+            const newCategory = new Category(
+                {
+                    name: 'default',
+                    description: 'Default category for unclassified commentaries'
+                }
+            )
+            await newCategory.save() 
+        }
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error whit default category',
+                err
+            }
+        )
+    }
+}
+
+export const createCategory = async(req, res) =>{
+    try {
+        
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error creating a category',
+                err
+            }
+        )
+    }
+}
