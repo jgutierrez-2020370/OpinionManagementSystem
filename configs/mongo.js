359b7c731652236a1464fdd0e2ba9a09deba741b
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-16 22:40:56 -0600

    Set up initial project structure with base files and directories

---
+++ configs/mongo.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+export const connect = async()=>{
+    try {
+        mongoose.connection.on('error', ()=>{
+            console.log('Mongo DB | cold not be conected to mongooseDB')
+        })
+        mongoose.connection.on('connecting', ()=>{
+            console.log('Mongo DB | try connecting')
+        })
+        mongoose.connection.on('connected', ()=>{
+            console.log('Mongo DB | connected to mongoose')
+        })
+        mongoose.connection.on('open', ()=>{
+            console.log('Mongo DB | connected to database')
+        })
+        mongoose.connection.on('reconnected', ()=>{
+            console.log('Mongo DB | reconnected to mongoDB')
+        })
+        mongoose.connection.on('disconnected', ()=>{
+            console.log('Mongo DB | disconnected')
+        })
+
+        await mongoose.connect(
+            `${process.env.DB_SERVICE}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+            {
+                maxPoolSize: 50,
+                serverSelectionTimeoutMS: 5000
+            }
+        )
+    } catch (err) {
+        console.logd('Database connection failed' , err)
+    }
+}
---
+++ src/user/user.model.js
@@ -0,0 +1,46 @@
+import { Schema } from "mongoose";
+
+const userSchema = Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+            maxLength: [50, `Can't be oversize 50 characters`]
+        },
+        description: {
+            type: String,
+            required: [true, 'Your description is required'],
+            maxLength: [75, `Can't be oversize 75 characters`]
+        },
+        userName: {
+            type: String,
+            required: [true, 'Your user name is required'],
+            unique: true,
+            lowercase: true,
+            maxLength: [15, `Can't be oversize 15 characters`]
+        },
+        email: {
+            type: String,
+            required: [true , 'email is required'],
+            unique: true 
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minLength: [8, `Password must be 8 characteres`],
+            maxLength: [100, `Can't be oversize 16 characteres`],
+            match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm]
+        }
+    },
+    {
+        versionKey: false, 
+        timeStamps: true
+    }
+)
+
+userSchema.methods.toJSON = function(){
+    const { __v, password, ...user} = this.toObject()
+    return user
+}
+
+export default model('User', userSchema)
