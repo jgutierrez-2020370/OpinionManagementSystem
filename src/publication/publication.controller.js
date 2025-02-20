904b24140742f7ac8d5e349a56ddd40b77eaf3fe
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-19 23:51:13 -0600

    Create and update functions added

---
+++ src/publication/publication.controller.js
@@ -0,0 +1,139 @@
+import Category from '../category/category.model.js'
+import Publication from './publication.model.js'
+import User from '../user/user.model.js'
+
+export const createPublication = async (req, res) => {
+    try {
+        let { category, creator ,...data } = req.body
+
+        if (!creator) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: "Creator is required"
+                }
+            )
+        }
+
+        const creatorData = await User.findOne({ _id: creator})
+        
+        if(!creatorData) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: "Creator not found"
+                }
+            )
+        }
+
+        if (!category) {
+            const categoryDefault = await Category.findOne({ name: "default"})
+            category = categoryDefault.id
+        }
+
+        const categoryData = await Category.findOne({ _id: category})
+
+        if (!categoryData) {
+            return res.status(404).send(
+                {
+                success: false,
+                message: "Category not found"
+                }
+            )
+        }
+
+        const publication = new Publication(
+            {
+                ...data,
+                category: [categoryData.id],
+                creator: [creatorData.id]
+            }
+        )
+        await publication.save()
+        return res.status(200).send(
+            {
+                success: true,
+                message: 'Publication added succesfully'
+            }
+        )
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error creating a publication',
+                err
+            }
+        )
+        
+    }
+}
+
+
+export const updatePublication = async (req, res) => {
+    const { id } = req.params
+    const data = req.body
+
+    try {
+        const publication = await Publication.findById(id)
+        if (!publication) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'Publication not found'
+                }
+            )
+        }
+
+        if (publication.creator !== req.user.uid) {
+            return res.status(403).send(
+                {
+                    success: false,
+                    message: 'You are not allowed to update this publication'
+                }
+            )
+        }
+
+        if (data.category) {
+            const categoryData = await Category.findById(data.category)
+            if (!categoryData) {
+                return res.status(404).send(
+                    {
+                        success: false,
+                        message: 'Category not found'
+                    }
+                )
+            }
+        }
+
+        if (data.creator) {
+            return res.status(403).send(
+                {
+                    success: false,
+                    message: 'Creator cannot be updated'
+                }
+            )
+        }
+
+        const updatedPublication = await Publication.findByIdAndUpdate(
+            id,
+            data,
+            { new: true }
+        )
+
+        return res.send({
+            success: true,
+            message: 'Publication updated successfully',
+            updatedPublication
+        })
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({
+            success: false,
+            message: 'General error when updating publication',
+            err
+        })
+    }
+}
+
