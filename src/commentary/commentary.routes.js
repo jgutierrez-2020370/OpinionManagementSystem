b6c5168bd1d1a7ff05e8d290b138ff44d7929b8d
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-22 17:45:12 -0600

    Added routes for getting and deleting comments

--- src/commentary/commentary.routes.js
+++ src/commentary/commentary.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express"
 import { validateJwt } from "../../middlewares/validate.jwt.js"
-import { createCommentary } from "./commentary.controller.js"
+import { createCommentary, deleteCommentary, getMyCommentaries, updateCommentary } from "./commentary.controller.js"
 
 const api = Router()
+
+api.get(
+    '/',
+    [
+        validateJwt
+    ],
+    getMyCommentaries
+)
 
 api.post(
     '/:id',
@@ -10,6 +18,22 @@ api.post(
         validateJwt
     ],
     createCommentary
+)
+
+api.put(
+    '/:id',
+    [
+        validateJwt
+    ],
+    updateCommentary
+)
+
+api.delete(
+    '/:id',
+    [
+        validateJwt
+    ],
+    deleteCommentary
 )
 
 export default api
